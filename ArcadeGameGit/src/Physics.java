6b725fb43be8cb2e1bc51486153bedd3637fb755
Skyler Cleland <clelansc@rose-hulman.edu>
2020-02-03 11:51:42 -0500

    Physics now has falling and collision detection

--- ArcadeGameGit/src/Physics.java
+++ ArcadeGameGit/src/Physics.java
@@ -1,4 +1,66 @@
 import javax.swing.*;
 
-public class Physics extends JComponent {
+public abstract class Physics{
+    private double x;
+    private double y;
+    private double height;
+    private double width;
+    private boolean isFalling;
+    private double vely;
+    private double fallAccel;
+
+    public Physics(double fallAccel){
+        this.fallAccel = fallAccel;
+    }
+
+    public boolean doesCollideWith(Physics p){
+        boolean doesContain = false;
+        if(this.contains(p.getX(), p.getY())) doesContain = true;
+        else if(this.contains(p.getX(), p.getY()+p.getHeight())) doesContain = true;
+        else if(this.contains(p.getX()+p.getWidth(), p.getY()+p.getHeight())) doesContain = true;
+        else if(this.contains(p.getX()+p.getWidth(), p.getY())) doesContain = true;
+        return doesContain;
+    }
+
+    public abstract void physicsCollision(Physics p);
+
+    public void setFalling(boolean isFalling){
+        this.isFalling = isFalling;
+    }
+
+    public void updatePos(double velyOveride){
+        if(isFalling){
+            vely += fallAccel;
+        }else{
+            vely = velyOveride;
+        }
+        y += vely;
+    }
+
+    public void updatePos(){
+        updatePos(0);
+    }
+
+    public boolean contains(double x, double y){
+        if(this.getX() <= x && x <= this.getX()+this.getWidth()){
+            return this.getY() <= y && y <= this.getY() + this.getHeight();
+        }
+        return false;
+    }
+
+    public double getY(){
+        return y;
+    }
+
+    public double getX(){
+        return x;
+    }
+
+    public double getWidth(){
+        return width;
+    }
+
+    public double getHeight(){
+        return height;
+    }
 }
