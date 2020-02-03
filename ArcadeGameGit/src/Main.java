8e5d261d12c24ddc1c52ea95ddb68604f93b015f
Skyler Cleland <clelansc@rose-hulman.edu>
2020-02-03 10:55:33 -0500

    edit main comment

--- ArcadeGameGit/src/Main.java
+++ ArcadeGameGit/src/Main.java
@@ -1,12 +1,7 @@
 /**
- * The main class for your arcade game.
+ * TODO:make a good comment here
  * 
- * You can design your game any way you like, but make the game start
- * by running main here.
- * 
- * Also don't forget to write javadocs for your classes and functions!
- * 
- * @author Buffalo
+ * @author Skyler Cleland and Daniel Vega
  *
  */
 public class Main {
@@ -15,7 +10,11 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Write your cool arcade game here!");
+		new Main();
+	}
+
+	public void Main(){
+
 	}
  
 }
