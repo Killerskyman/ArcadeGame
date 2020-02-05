2a3c6834f32b5a9bc16974ac9f03a6a59641c46d
Skyler Cleland <clelansc@rose-hulman.edu>
2020-02-05 01:44:19 -0500

    reflected changes in Movement

--- ArcadeGameGit/src/UserMovement.java
+++ ArcadeGameGit/src/UserMovement.java
@@ -1,2 +1,13 @@
+import java.awt.event.KeyEvent;
+
 public class UserMovement extends Movement {
+    
+    public UserMovement(Sprite sprite) {
+        super(sprite);
+    }
+    
+    @Override
+    public void updatePos() {
+    
+    }
 }
