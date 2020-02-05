5ca0f8430ba33373534cd41175941fe7011475b1
Skyler Cleland <clelansc@rose-hulman.edu>
2020-02-05 01:55:12 -0500

    added movement methods Movement

--- ArcadeGameGit/src/Movement.java
+++ ArcadeGameGit/src/Movement.java
@@ -1,13 +1,16 @@
 
 public abstract class Movement{
     
-    private Sprite sprite;
+    public Sprite sprite;
     
     public Movement(Sprite sprite){
         this.sprite = sprite;
     }
     
     public abstract void updatePos();
+    public abstract void moveLeft();
+    public abstract void moveRight();
+    public abstract void jump();
     
     public void setSprite(Sprite sprite) {
         this.sprite = sprite;
--- ArcadeGameGit/src/UserMovement.java
+++ ArcadeGameGit/src/UserMovement.java
@@ -1,6 +1,8 @@
-import java.awt.event.KeyEvent;
 
 public class UserMovement extends Movement {
+    
+    private static final double horzMovSpeed = 5;
+    private static final double jumpVel = 20;
     
     public UserMovement(Sprite sprite) {
         super(sprite);
@@ -8,6 +10,20 @@ public class UserMovement extends Movement {
     
     @Override
     public void updatePos() {
+    }
     
+    @Override
+    public void moveLeft() {
+        sprite.setX(sprite.getX()-horzMovSpeed);
+    }
+    
+    @Override
+    public void moveRight() {
+        sprite.setX(sprite.getX()+horzMovSpeed);
+    }
+    
+    @Override
+    public void jump() {
+        sprite.setJumpVely(jumpVel);
     }
 }
