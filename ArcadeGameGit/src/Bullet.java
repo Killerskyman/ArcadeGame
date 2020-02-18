9b488281fd2b883d448c1e4fde8fd99a5244a88c
Skyler Cleland <clelansc@rose-hulman.edu>
2020-02-17 19:39:23 -0500

    organized javadoc

--- ArcadeGameGit/src/Bullet.java
+++ ArcadeGameGit/src/Bullet.java
@@ -12,14 +12,16 @@ public class Bullet extends Sprite{
     private double yv;
     private Point2D.Double vector;
     private double vel;
+    
+    /**
+     * constructs a bullet class
+     * @param x position of sprite
+     * @param y position of sprite
+     * @param vel of the bullet
+     * @param vector for bullet to travel
+     */
     public Bullet(double x, double y, double vel, Point2D.Double vector) {
-    	/**
-    	 * constructs a bullet class
-    	 * @param x position of sprite
-    	 * @param y position of sprite
-    	 * @param velocity of the bullet
-    	 * @param direction for bullet to travel
-    	 */
+    	
         super(0, x, y, 10, 10, true);
         this.vector = vector;
         this.vel = vel;
@@ -29,40 +31,25 @@ public class Bullet extends Sprite{
         this.xv = (dx*vel)/vm;
         this.yv = (dy*vel)/vm;
         addMover(new Movement(this) {
-        	/**
-        	 * adds a mover to the bullet 
-        	 */
             @Override
             public void updatePos() {
-            	/**
-            	 * moves the x and y position of the bullet in the direction of the vector
-            	 */
                 sprite.setX(sprite.getX()+xv);
                 sprite.setY(sprite.getY()+yv);
             }
     
             @Override
             public void moveLeft() {
-            	/**
-            	 * moves bullet left
-            	 */
-        
+            
             }
     
             @Override
             public void moveRight() {
-            	/**
-            	 * moves bullet right
-            	 */
-        
+            
             }
     
             @Override
             public void jump() {
-            	/**
-            	 * updates bullet's vertical velocity
-            	 */
-        
+            
             }
     
             @Override
@@ -76,36 +63,40 @@ public class Bullet extends Sprite{
         this(x,y,15,vector);
     }
     
+    /**
+     * spawns the bullet sprite
+     */
     @Override
     public Sprite spawning() {
-    	/**
-    	 * spawns the bullet sprite
-    	 */
+    	
         return null;
     }
     
+    /**
+     * makes the bullet unable to interact with the given sprite
+     * @param otherSprite sprite
+     */
     @Override
     public boolean interactsWith(Sprite otherSprite) {
-    	/**
-    	 * makes the bullet unable to interact with the given sprite
-    	 * @param other sprite
-    	 */
+    	
         return false;
     }
     
+    /**
+     * returns height to determine if hero dies
+     */
     @Override
     public double getJoustHeight() {
-    	/**
-    	 * returns height to determine if hero dies
-    	 */
+    	
         return 0;
     }
     
+    /**
+     * removes the bullet object
+     */
     @Override
     public void death() {
-    	/**
-    	 * removes the bullet object
-    	 */
+    
     
     }
 }
